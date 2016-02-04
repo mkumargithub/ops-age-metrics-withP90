bc94e154176fd047bd18cc90652156d33268a0f7
mkumaromnifone <mkumar@omnifone.com>
2016-02-04 14:30:01 +0000

    Initial commit

---
+++ metrics-core/src/test/java/io/dropwizard/metrics/MetricFilterTest.java
@@ -0,0 +1,14 @@
+package io.dropwizard.metrics;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class MetricFilterTest {
+    @Test
+    public void theAllFilterMatchesAllMetrics() throws Exception {
+        assertThat(MetricFilter.ALL.matches(MetricName.build(""), mock(Metric.class)))
+                .isTrue();
+    }
+}
